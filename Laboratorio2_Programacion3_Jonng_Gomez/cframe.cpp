944f30538b8b22788b2659f4ab9fd2ffdc3db519
Jonny Gomez <jonnygomez@unitec.edu>
2024-07-26 16:20:47 -0600

    Repo

--- Laboratorio2_Programacion3_Jonng_Gomez/cframe.cpp
+++ Laboratorio2_Programacion3_Jonng_Gomez/cframe.cpp
@@ -1,5 +1,7 @@
 #include "cframe.h"
 #include "ui_cframe.h"
+#include <QCoreApplication>
+#include <QApplication>
 
 cframe::cframe(QWidget *parent)
     : QMainWindow(parent)
@@ -13,8 +15,88 @@ cframe::~cframe()
     delete ui;
 }
 
+
+
+
 void cframe::on_pB_Ex1_clicked()
 {
-    QTabWidget->QTabWidget.setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void cframe::on_pB_Ex2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void cframe::on_pB_Ex3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void cframe::on_pB_Close_clicked()
+{
+   QCoreApplication::quit();
+}
+
+void cframe::on_SB_Fila_valueChanged(int arg1)
+{
+    int height = ui->SB_Fila->value();
+
+    QString triangulo;
+    int width = 2 * height - 1;
+
+    for (int i = 1; i <= height; ++i) {
+        int Asteriscos = 2 * i - 1;
+
+        int Espacios = (width - Asteriscos) / 2;
+
+        triangulo += QString(Espacios, ' ');
+        triangulo += QString(Asteriscos, '*');
+        triangulo += '\n';
+    }
+
+    ui->txt_Mostrar->setText(triangulo);
+}
+
+
+void cframe::on_PB_calcular_clicked()
+{
+
+
+
+    int suma = 0;
+    bool control = false;
+    bool validez = false;
+
+    QString numero = ui->num_tarjeta->text();
+    int tamaño = numero.length();
+
+
+    for (int i = tamaño - 1; i >= 0; --i) {
+        int digito = numero[i].digitValue();
+
+        if (control) {
+            digito *= 2;
+            if (digito > 9) {
+                digito = (digito / 10) + (digito % 10);
+            }
+        }
+
+        suma += digito;
+        control = !control;
+    }
+
+
+    if (suma % 10 == 0) {
+        validez = true;
+        ui->txt_mostrar->setText("Su Tarjeta es Valida");
+    } else {
+        validez = false;
+        ui->txt_mostrar->setText("Su Tarjeta NO es Valida");
+    }
+        // ui->txt_mostrar->setText("La tarjeta es inválida.");
+
+
 }
 
--- Laboratorio2_Programacion3_Jonng_Gomez/cframe.h
+++ Laboratorio2_Programacion3_Jonng_Gomez/cframe.h
@@ -17,8 +17,21 @@ public:
     cframe(QWidget *parent = nullptr);
     ~cframe();
 
+
+
 private slots:
     void on_pB_Ex1_clicked();
+
+    void on_pB_Ex2_clicked();
+
+    void on_pB_Ex3_clicked();
+
+    void on_pB_Close_clicked();
+
+
+    void on_SB_Fila_valueChanged(int arg1);
+
+    void on_PB_calcular_clicked();
 
 private:
     Ui::cframe *ui;
